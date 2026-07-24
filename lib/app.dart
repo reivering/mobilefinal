e57b1b5759006503c61d9b0f3ea1cb8bff9d2f64
Yeoh Shaun Whye <75309484+Itzshaunyeo@users.noreply.github.com>
2026-07-25 02:25:12 +0800

    chore: remove unused app shell colors

--- lib/app.dart
+++ lib/app.dart
@@ -3,12 +3,8 @@ import 'package:flutter/material.dart';
 import 'screens/budget_screens.dart';
 
 const _canvas = Color(0xFF160A1B);
-const _deepPurple = Color(0xFF2B1B37);
-const _purple = Color(0xFF783A99);
 const _purpleLight = Color(0xFF964FB6);
-const _muted = Color(0xFFAEAEB2);
 const _cardBlack = Color(0xFF111111);
-const _green = Color(0xFF00AE67);
 
 class BudgetTrackerApp extends StatelessWidget {
   const BudgetTrackerApp({super.key});
